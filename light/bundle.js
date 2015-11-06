7de2578c7959ff7ad613d1f79abca401c73b3945
Caner Uguz <caneruguz@gmail.com>
2015-11-06 17:07:51 -0500

    Add optimizing view

--- light/bundle.js
+++ light/bundle.js
@@ -3086,34 +3086,33 @@ var Treebeard = function (data, options) {
 
 Treebeard.prototype.init = function () {
     var tb = this;
-    console.log(this.options);
     console.log(this.data);
-    tb.totalItems = Math.ceil(tb.options.height / (tb.options.rowHeight)) + 2; // +1 for bottom border +2 for additional 2
+    tb.totalItems = Math.ceil(tb.options.height / (tb.options.rowHeight)) + 3; // +1 for bottom border +2 for additional 2
 
     var totalHeight = tb.data.length * (tb.options.rowHeight);
-    $(tb.options.container).append('<div class="tb-inner" style="height:' + totalHeight + 'px"></div>');
+    $(tb.options.container)
+        .append('<div class="tb-inner" style="height:' + totalHeight + 'px"><div class="tb-row-container"></div></div>');
 
     tb.range = {begin : 0, end: tb.totalItems};
     tb.marginTop = 0;
     tb.view();
 
-    $(tb.options.container).scroll(function(){
-        var $this = $(this);
-        var scrollTop = $this.scrollTop();
-        console.log(scrollTop);
-        var lastHidden = Math.floor(scrollTop / tb.options.rowHeight);
-        tb.range = { begin: lastHidden, end : lastHidden + tb.totalItems};
-        tb.marginTop = lastHidden * tb.options.rowHeight;
-        tb.view();
-    });
+    $(tb.options.container)
+        .scroll(function(){
+            var $this = $(this);
+            var scrollTop = $this.scrollTop();
+            var lastHidden = Math.floor(scrollTop / tb.options.rowHeight);
+            tb.range = { begin: lastHidden, end : lastHidden + tb.totalItems};
+            tb.marginTop = lastHidden * tb.options.rowHeight;
+            tb.view();
+        });
 };
 
 Treebeard.prototype.view = function() {
     var tb = this;
     var i;
-
+    $(tb.options.container + '> .tb-inner > .tb-row-container').html('');
     for (i = tb.range.begin; i < tb.range.end; i++) {
-        console.log(tb.range.begin, tb.range.end, tb.data[i].title);
         tb.buildRow(tb.data[i]);
     }
 };
@@ -3130,8 +3129,8 @@ Treebeard.prototype.buildRow = function(row) {
         //}
         innerTemplate.push('<div class="tb-col" style="width:' + col.width + '">'  + row[col.data] + '</div>');
     });
-    $(tb.options.container + '> .tb-inner')
-        .css('margin-top', tb.marginTop + 'px')
+    $(tb.options.container + '> .tb-inner > .tb-row-container')
+        .css('top', tb.marginTop + 'px')
         .append('<div class="tb-row" style="height:' + tb.options.rowHeight + 'px">' + innerTemplate.join('') + '</div>');
 };
 
